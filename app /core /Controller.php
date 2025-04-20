52ac94688c90204252b60b3dbde20f3b5d201429
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-20 05:20:21 -0300

    Update Controller.php

--- app /core /Controller.php
+++ app /core /Controller.php
@@ -1,1 +1,14 @@
+<?php
 
+class Controller {
+    public function view($view, $data = []) {
+        require_once __DIR__ . '/../views/' . $view . '.php';
+    }
+
+    public function model($model) {
+        require_once '../app/models/' . $model . '.php';
+        return new $model();
+    }
+}
+
+?>
