c99743337374b232ee9f2e7007d474aed010ee64
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-20 04:47:24 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Carrega as classes essenciais
+require_once 'app/core/App.php';
+require_once 'app/core/Controller.php';
+
+// Cria o objeto da aplicação e inicia o roteamento
+$app = new App();
+
+?>
