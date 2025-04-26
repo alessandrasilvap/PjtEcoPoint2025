9261aefcfd32fdb31438de8ac3250817644c5661
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-26 01:57:41 -0300

    Update OngsController.php

--- app/controllers/OngsController.php
+++ app/controllers/OngsController.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once __DIR__ . '/../middleware/AuthMiddleware.php';
+
 class OngsController extends Controller {
+    public function __construct() {
+        AuthMiddleware::check();
+    }
+
     public function index() {
         $this->view('ongs/index');
     }
