3011bb6c5046fba7abc87c44ec73880c193259db
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-26 02:00:57 -0300

    Update InformacoesController.php

--- app/controllers/InformacoesController.php
+++ app/controllers/InformacoesController.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once __DIR__ . '/../middleware/AuthMiddleware.php';
+
 class InformacoesController extends Controller {
+    public function __construct() {
+        AuthMiddleware::check();
+    }
+
     public function index() {
         $this->view('informacoes/index');
     }
