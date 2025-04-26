568937a3b94bfec686dfb83db09b7efec359914b
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-26 01:27:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <?php
 
-// Carrega as classes essenciais
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+//Carrega as classes essenciais
+require_once 'config/config.php';
 require_once 'app/core/App.php';
 require_once 'app/core/Controller.php';
 
-// Cria o objeto da aplicação e inicia o roteamento
+//Cria o objeto da aplicação e inicia o roteamento
 $app = new App();
-
-// $url = isset($_GET['url']) ? $_GET['url'] : 'home';
-
-// switch ($url) {
-//     case 'home':
-//         include 'app/views/home/index.php';
-//         break;
-//     case 'login':
-//         include 'app/views/login/index.php';
-//         break;
-//     case 'cadastro':
-//         include 'app/views/cadastro/index.php';
-//         break;
-//     default:
-//         echo "Página não encontrada!";
-// }
-
-
 ?>
-
-
