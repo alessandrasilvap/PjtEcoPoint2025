de6ae3b596eb9288dc982abbb04ce1707ac4372e
Alessandra Pereira <alessandracris393@gmail.com>
2025-04-26 01:56:59 -0300

    Update SobreController.php

--- app/controllers/SobreController.php
+++ app/controllers/SobreController.php
@@ -1,6 +1,13 @@
 <?php
 
+require_once __DIR__ . '/../middleware/AuthMiddleware.php';
+
+
 class SobreController extends Controller {
+    public function __construct() {
+        AuthMiddleware::check();
+    }
+
     public function index() {
         $this->view('sobre/index');
     }
