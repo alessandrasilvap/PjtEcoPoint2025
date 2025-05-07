5195359d2e014411ab04893f7acbccb1603d5e59
eric <rudion83@gmail.com>
2025-05-07 12:36:07 -0300

    voltando às configurações anteriores do mapa

--- app/views/mapa/index.php
+++ app/views/mapa/index.php
@@ -46,50 +46,7 @@
                 //L.map('map') = Inicializa o mapa dentro do div com id map
                 //setView([-23.550520, -46.633308], 12) define a posição inicial do mapa (coordenadas de um dos pontos de coleta de lixo eletrônico na zona sul do Rio) e o nível de zoom.
                 const map = L.map('map').setView([-22.976012, -43.229052], 8);
-                const pontos = [
-                    { coords: [-22.901118, -43.183435], nome: "RETIPEL" }, //Exemplo de ponto
-                    { coords: [-22.904817, -43.176462], nome: "SABOR SAUDE" },  //Outro ponto
-                    { coords: [-22.904982, -43.176313], nome: "T.T. BURGUER" },  //Outro ponto
-                    { coords: [-22.905054, -43.184149], nome: "SOLETO URUGUAIANA" },  //Outro ponto
-                    { coords: [-22.930665, -43.180084], nome: "Colégio EDEM" },  //Outro ponto
-                    { coords: [-22.984678, -43.219712], nome: "Loja Ahlma" },  //Outro ponto
-                    { coords: [-22.979325, -43.221055], nome: "Clube de Regatas Flamengo" },  //Outro ponto
-                    { coords: [-22.983368, -43.218491], nome: "Shopping Rio Design Leblon" },  //Outro ponto
-                    { coords: [-22.985545, -43.227639], nome: "T.T. Burguer" },  //Outro ponto
-                    { coords: [-22.953818, -43.190190], nome: "Galeria Info Zona Sul" },  //Outro ponto
-                    { coords: [-22.921872, -43.176173], nome: "NEX COWORKING" },  //Outro ponto
-                    { coords: [-22.951918, -43.184905], nome: "UNIRIO" },  //Outro ponto
-                    { coords: [-22.931480, -43.180927], nome: "REDE ASTA" },  //Outro ponto
-                    { coords: [-22.886616, -43.264636], nome: "COOPAMA" },  //Outro ponto
-                    { coords: [-22.832808, -43.347949], nome: "ACMR" },  //Outro ponto
-                    { coords: [-22.869763, -43.275592], nome: "COOPCAL" },  //Outro ponto
-                    { coords: [-22.874122, -43.282002], nome: "COOPGALEÃO" },  //Outro ponto
-                    { coords: [-22.918575, -43.237834], nome: "Maraca Hostel" },  //Outro ponto
-                    { coords: [-22.921548, -43.255970], nome: "One On One Coworking" },  //Outro ponto
-                    { coords: [-22.006827, -43.457705], nome: "Condominio Sublime" },  //Outro ponto
-                    { coords: [-22.999420, -43.344194], nome: "IJOKER no Shopping Bayside" },  //Outro ponto
-                    { coords: [-22.001479, -43.386056], nome: "Shopping Rio Design Barra" },  //Outro ponto
-                    { coords: [-22.007394, -43.323952], nome: "ASSOCIAÇÃO BOSQUE MARAPENDI" },  //Outro ponto
-                    { coords: [-22.010545, -43.304444], nome: "T.T. BURGUER" },  //Outro ponto
-                    { coords: [-22.023706, -43.481743], nome: "ESTACIONAMENTO TERREIRÃO" },  //Outro ponto
-                    { coords: [-22.002490, -43.350012], nome: "VEZPA - PARQUE DAS ROSAS" },  //Outro ponto
-                    { coords: [-22.003301, -43.321321], nome: "ICAIU - CITTÁ AMÉRICA" },  //Outro ponto
-                    { coords: [-25.48813, -49.34549], nome: "Ecoponto Caiuá" },  //Outro ponto
-                    { coords: [-25.46293, -49.19528], nome: "Ecoponto Cajuru" },  //Outro ponto
-                    { coords: [-25.58104, -49.32730], nome: "Ecoponto Campo de Santana" },  //Outro ponto
-                    { coords: [-25.51209, -49.33182], nome: "Ecoponto CIC" },  //Outro ponto
-                    { coords: [-25.53202, -49.25127], nome: "Ecoponto Érico Veríssimo" },  //Outro ponto
-                    { coords: [-25.55015, -49.25444], nome: "Ecoponto Guaçuí" },  //Outro ponto
-                    { coords: [-25.49152, -49.20291], nome: "Ecoponto Icaraí" },  //Outro ponto
-                    { coords: [-25.55455, -49.24470], nome: "Ecoponto Jandaia" },  //Outro ponto
-                    { coords: [-25.42127, -49.36383], nome: "Ecoponto Metropolitano" },  //Outro ponto
-                    { coords: [-25.52940, -49.23031], nome: "Ecoponto Vila Nova" },  //Outro ponto
-                    { coords: [-25.50299, -49.22663], nome: "DMT Reciclagem" },  //Outro ponto
-                    { coords: [-26.22782, -52.67011], nome: "PREFEITURA DE PATO BRANCO" },  //Outro ponto
-                    { coords: [-8.03763, -34.94252], nome: "Reeecicle" },  //Outro ponto
-                    { coords: [-23.51544, -46.59045], nome: "Rapidosucata" },  //Outro ponto
-                    { coords: [-23.51524, -46.59048], nome: "Rapidosucata" }  //Outro ponto
-                ];
+    
                 //Adiciona uma camada de tile (mapa base), conjuntos de dados geoespaciais disponibilizados gratuitamente
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
@@ -119,10 +76,17 @@
                     console.warn('Geolocalização não suportada por este navegador.');
                 }
     
-                pontos.forEach(ponto => {
-                 const marker = L.marker(ponto.coords).addTo(map);
-                 marker.bindPopup(ponto.nome);
-                });
+                fetch('api/pontosAprovados.php')
+                    .then(response => response.json())
+                    .then(pontos => {
+                        pontos.forEach(ponto => {
+                            const marker = L.marker([ponto.latitude, ponto.longitude]).addTo(map);
+                            marker.bindPopup(ponto.nome);
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Erro ao carregar pontos do mapa:', error);
+                    });
             </script>
             <form action="<?= BASE_URL ?>/mapa/cadastrarPonto" method="POST" id="formColeta" name="formulariocoleta" onsubmit="validarPontocoleta(event)">
                 <div class="cadastro">
